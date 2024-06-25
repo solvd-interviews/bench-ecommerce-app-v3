087122775160cf0ea7a23d07035ed1d3b91068e6
Bruno Membrado <brunomembrado10@gmail.com>
2024-06-25 08:54:20 -0300

    next.js 14 with tailwindcss + typescript

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Solvd Ecommerce",
+  description: "Created by QA & bench full stack team with Next.js 14.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <footer></footer>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header>Solvd Ecommerce</header>;
+};
+
+export default Header;
