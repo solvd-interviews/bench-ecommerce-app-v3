c76135242a4bdfa2a9783dac8ffeca704969efb9
Bruno Membrado <101297827+brunomembrado@users.noreply.github.com>
2024-07-25 11:29:03 -0300

    bug fixed (#4)

--- src/app/api/generateData/route.ts
+++ src/app/api/generateData/route.ts
@@ -1,17 +1,32 @@
+import { config } from "@/lib/auth";
 import { sampleData } from "@/lib/data";
 import dbConnect from "@/lib/dbConnect";
 import CounterModel from "@/lib/models/CounterModel";
+import OrderModel from "@/lib/models/OrderModel";
 import ProductModel from "@/lib/models/ProductModel";
 import UserModel from "@/lib/models/UserModel";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
+  const { user } = await getServerSession(config);
+
+  if (!user || !user.isAdmin) {
+    return Response.json(
+      { message: "unauthorized" },
+      {
+        status: 401,
+      }
+    );
+  }
+
   const { users, products } = sampleData;
   await dbConnect();
 
   await UserModel.deleteMany();
   await ProductModel.deleteMany();
   await CounterModel.deleteMany();
+  await OrderModel.deleteMany();
 
   // Save users with incremental userNumber sequentially
   for (const user of users) {
--- src/lib/models/CounterModel.ts
+++ src/lib/models/CounterModel.ts
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-const counterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 0 },
-});
+const counterSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    seq: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const CounterModel =
   mongoose.models.Counter || mongoose.model("Counter", counterSchema);
