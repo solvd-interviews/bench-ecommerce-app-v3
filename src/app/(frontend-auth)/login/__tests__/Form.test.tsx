cf5cc0212f8d8d344eb46cd3c61bf614e41908be
Peter <pedromoyano454@gmail.com>
2024-08-15 18:59:20 -0300

    Fix next/navigation mock

--- src/app/(frontend-auth)/login/__tests__/Form.test.tsx
+++ src/app/(frontend-auth)/login/__tests__/Form.test.tsx
@@ -17,9 +17,13 @@ jest.mock('next-auth/react', () => ({
 jest.mock('next/navigation', () => ({
   useRouter: () => ({
     replace: jest.fn(),
-    push: jest.fn()
+    push: jest.fn(),
+  }),
+  useSearchParams: () => ({
+    get: jest.fn().mockReturnValue(null),
   }),
 }));
+
 
 
 describe('Login Form', () => {
