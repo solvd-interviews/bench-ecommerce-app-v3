b089724e137abcece2dfe79d14f03bde8fd8f6f4
Bruno Membrado <brunomembrado10@gmail.com>
2024-08-15 17:35:33 -0300

    clg cleared

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 
 export function middleware(request: Request) {
   // Store current request url in a custom header, which you can read later
-  console.log("middleware");
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-url", request.url);
 
