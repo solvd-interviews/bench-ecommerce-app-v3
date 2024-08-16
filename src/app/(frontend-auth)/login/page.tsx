dfdb0148489db745b53cbf3dac279d5537a36e71
Bruno Membrado <brunomembrado10@gmail.com>
2024-08-16 11:49:29 -0300

    sign in check

--- src/app/(frontend-auth)/login/page.tsx
+++ src/app/(frontend-auth)/login/page.tsx
@@ -1,11 +1,5 @@
-import { getServerSession } from 'next-auth';
-import Form from './Form';
-import { redirect } from 'next/navigation';
+import Form from "./Form";
 
 export default async function LoginPage() {
-  const session = await getServerSession();
-  if (session) {
-    redirect('/');
-  }
   return <Form />;
 }
