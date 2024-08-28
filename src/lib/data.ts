1e4ade4661c07a77ae371bed935c1fe90ce742c2
Bruno Membrado <brunomembrado10@gmail.com>
2024-08-28 09:52:59 -0300

    migration v3

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -9,7 +9,7 @@ export const sampleData = {
         "Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.",
       price: 29.99,
       images: [
-        "https://res.cloudinary.com/doxpkairh/image/upload/v1721052541/bag_f8z6gw_wqtxic.png",
+        "https://res.cloudinary.com/doxpkairh/image/upload/v1724847902/bag_f8z6gw_wqtxic_igfvsk.png",
       ],
       isBlocked: false,
       stock: 10,
@@ -21,7 +21,7 @@ export const sampleData = {
         "A red light isn't the desired state in testing but it sure helps",
       price: 9.99,
       images: [
-        "https://res.cloudinary.com/doxpkairh/image/upload/v1721052541/bike-light_snzdnk_wlbrng.png",
+        "https://res.cloudinary.com/doxpkairh/image/upload/v1724847903/bike-light_snzdnk_wlbrng_lmhz9o.png",
       ],
       isBlocked: false,
       stock: 5,
@@ -33,7 +33,7 @@ export const sampleData = {
         "Get your testing superhero on with the Sauce Labs bolt T-shirt.",
       price: 15.99,
       images: [
-        "https://res.cloudinary.com/doxpkairh/image/upload/v1721052541/black-tshirt_mdghbp_w3l28m.png",
+        "https://res.cloudinary.com/doxpkairh/image/upload/v1724847904/black-tshirt_mdghbp_w3l28m_op00ux.png",
       ],
       isBlocked: true,
       stock: 10,
@@ -45,7 +45,7 @@ export const sampleData = {
         "It's not every day that you come across a midweight quarter-zip fleece jacket.",
       price: 49.99,
       images: [
-        "https://res.cloudinary.com/doxpkairh/image/upload/v1721052547/fleece-jacket_kznpkh_txr4pm.png",
+        "https://res.cloudinary.com/doxpkairh/image/upload/v1724847903/fleece-jacket_kznpkh_txr4pm_pwyjcn.png",
       ],
       isBlocked: false,
       stock: 10000,
@@ -57,7 +57,7 @@ export const sampleData = {
         "Rib snap infant onesie for the junior automation engineer in development.",
       price: 7.99,
       images: [
-        "https://res.cloudinary.com/doxpkairh/image/upload/v1721052543/onesie_qva38p_zzev9t.png",
+        "https://res.cloudinary.com/doxpkairh/image/upload/v1724847903/onesie_qva38p_zzev9t_lhjxgz.png",
       ],
       isBlocked: false,
       stock: 10,
@@ -69,7 +69,7 @@ export const sampleData = {
         "This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard.",
       price: 15.99,
       images: [
-        "https://res.cloudinary.com/doxpkairh/image/upload/v1721052542/red-tshirt_ygrp5h_mkviue.png",
+        "https://res.cloudinary.com/doxpkairh/image/upload/v1724847903/red-tshirt_ygrp5h_mkviue_lnn3iy.png",
       ],
       isBlocked: false,
       stock: 10,
