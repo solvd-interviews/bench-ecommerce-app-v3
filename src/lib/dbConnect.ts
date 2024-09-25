f8ff4cbdd0966f2fa08e0428e96bc30d121f134e
Bruno Membrado <brunomembrado10@gmail.com>
2024-09-25 10:55:24 -0300

    code cleaned

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,15 +1,5 @@
 import mongoose from "mongoose";
 
-/* async function dbConnect() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI!);
-  } catch (error) {
-    throw new Error("Connection failed!");
-  }
-}
-
-export default dbConnect; */
-
 // Singleton class for the database connection
 export class DatabaseSingleton {
   private static instance: DatabaseSingleton;
@@ -28,14 +18,12 @@ export class DatabaseSingleton {
   // Connect to the database if not already connected
   public async connect() {
     if (this.isConnected) {
-      console.log("Already connected to the database.");
       return;
     }
 
     try {
       await mongoose.connect(process.env.MONGODB_URI!);
       this.isConnected = true;
-      console.log("Database connection established.");
     } catch (error) {
       console.error("Database connection failed!", error);
       throw new Error("Connection failed!");
