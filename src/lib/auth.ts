6acf7e154a4590f83266346d7c07abc08bccf33a
Pedro Ignacio Moyano <86992011+petermoyano@users.noreply.github.com>
2024-10-01 16:23:40 -0300

    Update src/lib/auth.ts
    Co-authored-by: Andrés Virga <andvirga@gmail.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -28,7 +28,7 @@ export const config = {
           typeof email !== "string" ||
           email.length < 3 ||
           typeof password !== "string" ||
-          password.length < 3
+          password.length < 8
         ) {
           throw new Error("Invalid email or password.");
         }
