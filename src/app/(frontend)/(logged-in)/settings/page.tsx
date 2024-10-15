e8d9afcaa30b41219f0c06b67c2b2250b496d053
Bruno Membrado <brunomembrado10@gmail.com>
2024-10-15 14:36:43 -0300

    version one /settings

---
+++ src/app/(frontend)/(logged-in)/settings/page.tsx
@@ -0,0 +1,293 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+import { useForm, SubmitHandler, UseFormRegister } from "react-hook-form";
+import { toast } from "sonner";
+
+interface NameData {
+  name: string;
+}
+
+interface EmailData {
+  email: string;
+}
+
+interface PasswordData {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+}
+
+const Page = () => {
+  const { data, update } = useSession();
+  console.log("data ", data);
+
+  // Name form setup
+  const {
+    register: registerName,
+    handleSubmit: handleSubmitName,
+    setValue: setValueName,
+    formState: { errors: errorsName, isSubmitting: isSubmittingName },
+  } = useForm<NameData>({
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  // Email form setup
+  const {
+    register: registerEmail,
+    handleSubmit: handleSubmitEmail,
+    setValue: setValueEmail,
+    formState: { errors: errorsEmail, isSubmitting: isSubmittingEmail },
+  } = useForm<EmailData>({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  // Password form setup
+  const {
+    register: registerPassword,
+    handleSubmit: handleSubmitPassword,
+    formState: { errors: errorsPassword, isSubmitting: isSubmittingPassword },
+    watch: watchPass,
+  } = useForm<PasswordData>({
+    defaultValues: {
+      currentPassword: "",
+      newPassword: "",
+      confirmPassword: "",
+    },
+  });
+
+  const name = data?.user?.name;
+  const email = data?.user?.email;
+
+  // Set default values from session
+  useEffect(() => {
+    if (name) setValueName("name", name);
+    if (email) setValueEmail("email", email);
+  }, [name, email, setValueName, setValueEmail]);
+
+  if (!data) {
+    return <span className="loading loading-spinner loading-lg"></span>;
+  }
+
+  // Submit handlers
+  const onSubmitName: SubmitHandler<NameData> = async (formData) => {
+    try {
+      const response = await fetch("/api/me", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to update name");
+      }
+      toast.success(`Name updated`);
+      await update({ name: formData.name });
+    } catch (error) {
+      toast.error(`Error updating name`);
+    }
+  };
+
+  const onSubmitEmail: SubmitHandler<EmailData> = async (formData) => {
+    try {
+      const response = await fetch("/api/me", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to update email");
+      }
+      toast.success(`Email updated`);
+      await update({ email: formData.email });
+    } catch (error) {
+      toast.error(`Error updating email`);
+    }
+  };
+
+  const onSubmitPassword: SubmitHandler<PasswordData> = async (formData) => {
+    const { newPassword, confirmPassword, currentPassword } = formData;
+
+    // Compare newPassword and confirmPassword
+    if (newPassword !== confirmPassword) {
+      toast.error("New passwords do not match");
+      return; // Early return if passwords do not match
+    }
+
+    try {
+      const response = await fetch("/api/me", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          currentPassword,
+          newPassword,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to update password");
+      }
+      toast.success("Password updated successfully!");
+    } catch (error) {
+      toast.error(`Error updating password`);
+    }
+  };
+
+  // InputField component
+  const InputField = <T extends Record<string, any>>({
+    id,
+    label,
+    type = "text",
+    required,
+    minLength,
+    validate,
+    registerFn,
+    errors,
+  }: {
+    id: keyof T;
+    label: string;
+    type?: string;
+    required?: boolean;
+    minLength?: number;
+    validate?: (value: string) => string | boolean;
+    registerFn: any;
+    errors: any;
+  }) => (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id as string} className="text-xl">
+        {label}
+      </label>
+      <input
+        id={id as string}
+        type={type}
+        className="input input-bordered grow"
+        {...registerFn(id, {
+          required: required && `${label} is required`,
+          minLength: minLength && {
+            value: minLength,
+            message: `${label} must be at least ${minLength} characters`,
+          },
+          validate,
+        })}
+      />
+      {errors[id] && <p className="text-red-500">{errors[id]?.message}</p>}
+    </div>
+  );
+
+  return (
+    <div className="p-4 w-full h-full flex flex-col gap-8">
+      <h2 className="text-4xl">Settings</h2>
+
+      <div className="flex flex-wrap gap-12 w-full h-full justify-around items-center">
+        {/* Name Section */}
+        <form
+          onSubmit={handleSubmitName(onSubmitName)}
+          className="flex flex-col gap-8 w-96 bg-white p-2 shadow-2xl rounded-md"
+        >
+          <div className="flex flex-col gap-2">
+            <h3 className="text-2xl">Change your name</h3>
+            <InputField<NameData>
+              id="name"
+              label="Username"
+              required
+              minLength={3}
+              registerFn={registerName}
+              errors={errorsName}
+            />
+            <button
+              className="btn btn-neutral"
+              type="submit"
+              disabled={isSubmittingName}
+            >
+              {isSubmittingName ? "Saving..." : "Update Name"}
+            </button>
+          </div>
+        </form>
+
+        {/* Email Section */}
+        <form
+          onSubmit={handleSubmitEmail(onSubmitEmail)}
+          className="flex flex-col gap-8 w-96 bg-white p-2 shadow-2xl rounded-md "
+        >
+          <div className="flex flex-col gap-2">
+            <h3 className="text-2xl">Change your email</h3>
+            <InputField<EmailData>
+              id="email"
+              label="Email"
+              type="email"
+              required
+              validate={(value) =>
+                /\S+@\S+\.\S+/.test(value) || "Invalid email address"
+              }
+              registerFn={registerEmail}
+              errors={errorsEmail}
+            />
+            <button
+              className="btn btn-neutral"
+              type="submit"
+              disabled={isSubmittingEmail}
+            >
+              {isSubmittingEmail ? "Saving..." : "Update Email"}
+            </button>
+          </div>
+        </form>
+
+        <form
+          onSubmit={handleSubmitPassword(onSubmitPassword)}
+          className="flex flex-col gap-8 w-96 bg-white p-2 shadow-2xl rounded-md"
+        >
+          <div className="flex flex-col gap-2">
+            <h3 className="text-2xl">Change your password</h3>
+            <InputField<PasswordData>
+              id="currentPassword"
+              label="Current password"
+              type="password"
+              required
+              registerFn={registerPassword}
+              errors={errorsPassword}
+            />
+            <InputField<PasswordData>
+              id="newPassword"
+              label="New password"
+              type="password"
+              required
+              minLength={6}
+              registerFn={registerPassword}
+              errors={errorsPassword}
+            />
+            <InputField<PasswordData>
+              id="confirmPassword"
+              label="Confirm password"
+              type="password"
+              required
+              validate={
+                (value) =>
+                  value === watchPass("newPassword") || "Passwords do not match" // Use watch to get newPassword
+              }
+              registerFn={registerPassword}
+              errors={errorsPassword}
+            />
+            <button
+              className="btn btn-neutral"
+              type="submit"
+              disabled={isSubmittingPassword}
+            >
+              {isSubmittingPassword ? "Saving..." : "Change Password"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/api/me/route.ts
@@ -0,0 +1,99 @@
+import { config } from "@/lib/auth";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/lib/models/UserModel";
+import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { compareSync, hashSync } from "bcrypt";
+
+// Email validation regex
+const emailRegex = /\S+@\S+\.\S+/;
+
+// PATCH request handler
+export const PATCH = async (request: NextRequest) => {
+  // Get the session user
+  const session = await getServerSession(config);
+  if (!session?.user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const userId = session.user._id;
+
+  // Ensure the user ID is valid
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return new NextResponse(
+      JSON.stringify({ message: "User id is not valid" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  // Parse the request body
+  const body = await request.json();
+  const { name, email, currentPassword, newPassword } = body;
+
+  // Validate name, email, and password
+  if (name && name.length < 3) {
+    return new NextResponse(
+      JSON.stringify({ message: "Name must be at least 3 characters long." }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  if (email && !emailRegex.test(email)) {
+    return new NextResponse(
+      JSON.stringify({ message: "Invalid email address." }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  // Connect to the database
+  await dbConnect();
+
+  // Find the user by ID
+  const user = await UserModel.findById(userId);
+  if (!user) {
+    return new NextResponse(JSON.stringify({ message: "User not found" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  // Check if currentPassword is provided and validate it
+  if (currentPassword) {
+    const isMatch = compareSync(currentPassword, user.password);
+    if (!isMatch) {
+      return new NextResponse(
+        JSON.stringify({ message: "Current password is incorrect." }),
+        {
+          status: 401,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+  }
+
+  // Update fields if they exist
+  if (name) user.name = name;
+  if (email) user.email = email;
+  if (newPassword) user.password = hashSync(newPassword, 5); // Hash the new password
+
+  // Save the updated user document
+  await user.save();
+
+  // Return the updated user (without password)
+  const { password: _, ...updatedUser } = user.toObject(); // Exclude password
+
+  return new NextResponse(JSON.stringify({ user: updatedUser }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
